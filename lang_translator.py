1b69fc487c844dc6e1eed7c57ff1f30a4e770b12
Sparsh Agarwal <84501667+w41k3r70093@users.noreply.github.com>
2021-11-24 18:39:02 +0530

    Add files via upload

---
+++ lang_translator.py
@@ -0,0 +1,8 @@
+from googletrans import Translator
+
+translator = Translator()
+
+text =  str(input("Enter the string you want to translate it to --> "))
+
+translated = translator.translate(text, dest = 'hi')
+print(translated)
